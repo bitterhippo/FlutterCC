9aca69c75300e0f928b6df2d70d12d395b8e3d6f
Noah <bitterhippo@gmail.com>
2022-02-06 00:00:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MyApp());
 }
 
+//Every widget must call Stateless or Stateful + have a build method
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('This is my app'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('My first App')),
+        body: Text('Default text'),
+      ),
     );
   }
 }
