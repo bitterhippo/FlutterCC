72e8594fe9ce17d56371ba1ac7f03b8a36011b64
Noah <bitterhippo@gmail.com>
2022-02-06 00:31:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,33 @@ void main() {
   runApp(MyApp());
 }
 
-//Every widget must call Stateless or Stateful + have a build method
+//Every Widget must include:
+// A) Extension of Stateful / Stateless Component
+// B) a build method.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var questions = [
+      Text('What is your favorite color?'), 
+      Text('What is your favorite food?')
+      ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My first App')),
-        body: Text('Default text'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(child: Text('Answer1'),
+            onPressed: null),
+            ElevatedButton(child: Text('Answer2'),
+            onPressed: null),
+            ElevatedButton(child: Text('Answer3'),
+            onPressed: null)
+          ],
+        ),
       ),
-    );
+    );k
   }
 }
