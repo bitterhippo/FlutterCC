99776ab05c53ed638286ba8cffc418d0a43ea9dc
Noah <bitterhippo@gmail.com>
2022-02-07 08:11:52 +0900

    bug fix
    line 34 had an extra character in it that prevented everything from running.

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     var questions = [
-      Text('What is your favorite color?'), 
+      Text('What is your favorite color?'),
       Text('What is your favorite food?')
-      ];
+    ];
 
     return MaterialApp(
       home: Scaffold(
@@ -22,15 +21,12 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Text('The question!'),
-            ElevatedButton(child: Text('Answer1'),
-            onPressed: null),
-            ElevatedButton(child: Text('Answer2'),
-            onPressed: null),
-            ElevatedButton(child: Text('Answer3'),
-            onPressed: null)
+            ElevatedButton(child: Text('Answer1'), onPressed: null),
+            ElevatedButton(child: Text('Answer2'), onPressed: null),
+            ElevatedButton(child: Text('Answer3'), onPressed: null)
           ],
         ),
       ),
-    );k
+    );
   }
 }
